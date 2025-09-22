d91c6e29bbdab4be555f465e3800cb49d2b9668c
GiadaChiar <132461029+GiadaChiar@users.noreply.github.com>
2025-09-22 15:53:01 +0200

    new

--- js/style.js
+++ js/style.js
@@ -3,7 +3,28 @@
 
 const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 
+
 //message scroll front-end and back-end
 const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
 });
+
+
+//animate on scroll
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll("[data-animate]");
+
+    const observer = new IntersectionObserver((entries, obs) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                const el = entry.target;
+                const animationClass = el.dataset.animate;
+                el.classList.add("animate__animated", animationClass);
+                obs.unobserve(el); // animazione una volta sola
+            }
+        });
+    }, { threshold: 0.2 });
+
+    elements.forEach(el => observer.observe(el));
+});
+
