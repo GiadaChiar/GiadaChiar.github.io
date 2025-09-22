7b78fa99a179d9bacf2075788fb2d5c1bfdc2eef
GiadaChiar <132461029+GiadaChiar@users.noreply.github.com>
2025-09-22 13:44:08 +0200

    Messaggio del nuovo commit

--- js/style.js
+++ js/style.js
@@ -1,1 +1,9 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
+
+
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+
+//message scroll front-end and back-end
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
