687156e8ba13a829c5fadefb62b76bf765da62fd
GiadaChiar <132461029+GiadaChiar@users.noreply.github.com>
2025-09-23 15:35:17 +0200

    ultime modifiche

--- js/style.js
+++ js/style.js
@@ -1,4 +1,4 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
+//<scrip src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
 
 
 const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
@@ -27,4 +27,64 @@ document.addEventListener("DOMContentLoaded", () => {
 
     elements.forEach(el => observer.observe(el));
 });
+
+
+
+/*
+
+//how to scend email with account EmailJS
+
+emailjs.init("service_dejae9t");
+
+
+*/
+
+
+    // Inizializza EmailJS con la tua Public Key
+    //emailjs.init("LA_TUA_PUBLIC_KEY");
+    emailjs.init("CUSmzsjmmsxpQ0xR_");
+
+
+    
+//rules from forum
+//block number in the name field from forum
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("name").addEventListener("input", function () {
+    this.value = this.value.replace(/[0-9]/g, "");
+});
+
+//block letters in the phone field from forum
+
+document.getElementById("phone").addEventListener("input", function () {
+    this.value = this.value
+    .replace(/[^\d+]/g, '')
+    // allow only + at the beginning
+    .replace(/(?!^\+)\+/g, '');
+});
+});
+
+
+    //HOW TO SEND EMAIL WITH EMAILJS (from forum)
+    document.getElementById("contactForm").addEventListener("submit", function(e) {
+        e.preventDefault(); // blocca refresh
+
+        // Invia direttamente usando send (senza creare template nuovo)
+        emailjs.send("service_dejae9t", "template_abc123", {
+        user_name: document.getElementById("name").value,
+        user_company: document.getElementById("company").value,
+        user_phone: document.getElementById("phone").value,
+        user_email: document.getElementById("email").value,
+        user_subject: document.getElementById("subject").value,
+        message: document.getElementById("text-area").value
+        })
+        .then(() => {
+        alert("✅ Email inviata con successo!");
+        this.reset(); // clear form
+        })
+        .catch(err => {
+        alert("❌ Errore nell'invio: " + JSON.stringify(err));
+        });
+    });
 
